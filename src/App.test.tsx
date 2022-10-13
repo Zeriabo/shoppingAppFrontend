2f06b21953fb6926d82c8f5400a30bff773471b8
Zeriab Chiah <zeriab@hotmail.com>
2022-10-13 14:12:13 +0300

    docker

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
+
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './redux/app/store';
@@ -13,13 +13,5 @@ test('renders learn react link', () => {
   );
 
   expect(getByText(/learn/i)).toBeInTheDocument();
-=======
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
->>>>>>> 9a0e6a41ae837abd2ee8f9123c91947c2ef18251
 });
